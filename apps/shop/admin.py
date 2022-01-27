ca1dd2023fc0646219f6ecef14727e1111337af1
Sordun <sordungun@gmail.com>
2022-01-27 12:53:20 +0300

    доступ по id

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.http import HttpRequest
+
 from shop.models import CheckIn, Clients, Specialist
 
 
@@ -17,6 +20,25 @@ class SpecialistAdmin(admin.ModelAdmin):
 class ClientsAdmin(admin.ModelAdmin):
     list_display = ("user", "name", "car_model",)
     fields = ("name", "user", "car_model",)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        if not is_superuser:
+            form.base_fields["user"].disabled = True
+        return form
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        qs = super().get_queryset(request)
+
+        if request.user.is_superuser:
+            return qs
+
+        clients = request.user.clients
+        if request.user.id:
+            return qs.filter(id=clients.id)
+
+        return qs.none()
 
 
 admin.site.register(CheckIn)
